cf1e1720c477e8c41a36fff10d321a486871c132
pereiradaniel <danielpaulpereira@protonmail.com>
2021-02-12 14:03:26 -0500

    Upload multiple files.

--- W05/server.js
+++ W05/server.js
@@ -3,16 +3,16 @@ const exp = require("express");
 const app = exp();
 
 const multer = require("multer");
-const uploadImg = multer({dest: "./public/photos"});
+// const uploadImg = multer({dest: "./public/photos"});
 
 
-// const storage = multer.diskStorage({
-//     destination: "./public/photos",
-//     filename: function(req, file, cb) {
-//         cb(null, Date.now() + path.extname(file.originalname));
-//     }
-// });
-// const uploadImg = multer({storage: storage});
+const storage = multer.diskStorage({
+    destination: "./public/photos",
+    filename: function(req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+const uploadImg = multer({storage: storage});
 
 app.use(exp.static('public'));
 
@@ -26,9 +26,29 @@ app.get("/sign-up", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/userregister.html"));
 });
 
-app.post("/user-register", uploadImg.single("profile_img"), (req, res) => {
+const imgFiles = uploadImg.fields([
+    {name: "profile_img1", maxount: 1},
+    {name: "profile_img2", maxcount: 5},
+    {name: "profile_img3", maxcount: 1}
+]);
+
+app.post("/user-register", imgFiles, (req, res) => {
     console.log("User name: " + req.body.username);
-    console.log("File name: " + req.file.filename);
+    
+    for (var i = 0; i < req.files["profile_img1"].length; i++) {
+        console.log("File name: " + req.files["profile_img1"][i].filename);
+    }
+    
+    for (var i = 0; i < req.files["profile_img2"].length; i++) {
+        console.log("File name: " + req.files["profile_img2"][i].filename);
+    }
+    
+    
+    for (var i = 0; i < req.files["profile_img3"].length; i++) {
+        console.log("File name: " + req.files["profile_img3"][i].filename);
+    }
+
+    res.end();
 });
 
 app.listen(port);
